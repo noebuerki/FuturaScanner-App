44a50cb066facda8bdf56365aa1989368396e126
Bürki Noé <noe@buerki.ch>
2021-09-06 21:02:57 +0200

    Version Updates

--- app/src/test/java/ch/buerki/futurascanner/ExampleUnitTest.java
+++ app/src/test/java/ch/buerki/futurascanner/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package ch.buerki.futurascanner;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
